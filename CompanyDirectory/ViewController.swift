1cdda3f23f1fd228a4f17cd904b84436f79d0dfd
obaya884 <taku3.884@gmail.com>
2021-02-17 14:01:12 +0900

    Initial Commit

---
+++ CompanyDirectory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompanyDirectory
+//
+//  Created by 大林拓実 on 2021/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
